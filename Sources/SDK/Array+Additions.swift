87aed31ed2a4ea920cceead74b1a1c8f1cbbafc7
Eric Rowe <eric.j.rowe@gmail.com>
2026-03-25 14:46:15 -0600

    Test

---
+++ Sources/SDK/Array+Additions.swift
@@ -0,0 +1,15 @@
+// The Swift Programming Language
+// https://docs.swift.org/swift-book
+
+import Foundation
+
+public extension Array {
+    subscript(safe index: Int) -> Element? {
+        guard index >= 0, index < self.count else { return nil }
+        return self[index]
+    }
+}
+
+public class AccessValueEnum {
+//    var valueEnum: ValueEnum?
+}
